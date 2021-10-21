6e3f4dfe0e4798523ed565e03434222702196f5d
Cuatindoy <nycuatindoy@misena.edu.co>
2021-10-20 22:15:44 -0500

    se agrega el Proyecto

---
+++ src/Components/Searcbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Searcbar = () => {
+    let search = "Pikaachu";
+    return (
+        
+        <div>
+
+         <div>
+           <input placeholder="Buscador Pokémon"/>
+        
+         </div>
+            {search}
+         <div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Searcbar
